3a3c5a9729f41c1fd52518e1fb1736acdcd51665
Roman Dronov <roman.dronov@softdev.pw>
2023-09-17 02:05:31 +0600

    feat: enhance ESLint rules for stricter type checking
    Adjusted ESLint configuration to apply stricter type checking and stylistic rules. Replaced recommended rules by 'strict-type-checked' rules and 'stylistic-type-checked' rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,17 +6,11 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["@typescript-eslint/eslint-plugin"],
-  extends: ["plugin:@typescript-eslint/recommended"],
+  extends: ["plugin:@typescript-eslint/strict-type-checked", "plugin:@typescript-eslint/stylistic-type-checked"],
   root: true,
   env: {
     node: true,
     jest: true,
   },
   ignorePatterns: [".eslintrc.js"],
-  rules: {
-    "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-  },
 };
