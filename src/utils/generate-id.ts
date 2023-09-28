7ef03eb199ddbe37e04192cb2789c6b6919c99e0
Roman D <nixooyase@gmail.com>
2023-09-28 22:06:15 +0600

    refactor: reset project

--- src/utils/generate-id.ts
+++
@@ -1,8 +0,0 @@
-import { customAlphabet } from "nanoid";
-
-export function generateId(): string {
-  return customAlphabet(
-    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    24,
-  )();
-}
