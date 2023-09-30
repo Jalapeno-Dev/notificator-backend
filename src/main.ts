403ec0cecd918fa3a62b1fa7427f89db49942190
Roman D <nixooyase@gmail.com>
2023-09-30 12:53:54 +0600

    perf: install fastify instead of express

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from "@nestjs/core";
 import { MainModule } from "./main.module";
+import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
 
 async function bootstrap() {
-  const app = await NestFactory.create(MainModule);
+  const app = await NestFactory.create<NestFastifyApplication>(MainModule, new FastifyAdapter());
   await app.listen(3000);
 }
 
